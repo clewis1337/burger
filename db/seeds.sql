db0f4c8753e3342d88a73193b7a9c647fcee6ce3
Craig Lewis <clewis1337@gmail.com>
2018-08-18 23:47:21 -0700

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO `burgers_db`.`burgers` (`id`, `burger_name`, `devoured`) VALUES ('1', 'Double Cheeseburger', '0');
+INSERT INTO `burgers_db`.`burgers` (`id`, `burger_name`, `devoured`) VALUES ('2', 'Big Mac', '0');
+INSERT INTO `burgers_db`.`burgers` (`id`, `burger_name`, `devoured`) VALUES ('3', 'Whopper', '0');
